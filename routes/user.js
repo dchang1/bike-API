7fb93bbf17cd80c5a31e7cea0f587389b7777dd1
dchang1 <dchang2@swarthmore.edu>
2018-06-27 10:17:00 -0400

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,76 +2,34 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var User = require('../models/user');
+var Bike = require('../models/bike');
+var Ride = require('../models/ride');
+var Campus = require('../models/campus');
 
 module.exports = function(passport) {
-	router.get('/users/:id', passport.authenticate('jwt', {session: false}), function(req, res) {
-		User.findById(req.params.id, function(err, user) {
-			if (err) {
-				throw err;
-			} else {
-				res.json({
-					"user": {
-						username: user.username,
-						firstName: user.firstName,
-						lastName: user.lastName,
-					}
-				})
-			}
-		})
-	})
-
-	router.put('/users/:id/block', passport.authenticate('jwt', {session: false }), function(req, res) {
-		User.findOneAndUpdate({_id: req.params.id}, {$push: {blockers: req.user._id}}, function(err, blockedUser) {
-			if (err) {
-				throw error;
-			} else {
-				User.findOneAndUpdate({_id: req.user._id}, {$push: {blocking: req.params.id}}, function(err, blockingUser) {
-					if (err) {
-						throw error;
-					} else {
-						res.json({success: true});
-					}
-				})
-			}
-		})
-	})
-
-	router.put('/users/:id/unblock', passport.authenticate('jwt', { session: false }), function(req, res) {
-		User.findOneAndUpdate({_id: req.params.id}, {$pull: {blockers: req.user._id}}, function(err, blockedUser) {
-			if (err) {
-				throw error;
-			} else {
-				User.findOneAndUpdate({_id: req.user._id}, {$pull: {blocking: req.params.id}}, function(err, blockingUser) {
-					if (err) {
-						throw error;
-					} else {
-						res.json({success: true});
-					}
-				})
-			}
-		})
-	})
-
-
-
-
 	router.get('/user', passport.authenticate('jwt', { session: false }), function(req, res) {
-		User.findById(req.user._id).populate({path: 'notifications', options: {limit:1}}).exec(function(err, user) {
-			if (err)  {
-				throw err;
-			} else {
-			  res.json({
-				  "user": {
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            major: user.major || "",
-            classYear: user.classYear || ""
-          }
-		    })
-			}
+		User.findOne({email: req.user.email}, function(err, user) {
+			if(err) throw err;
+			res.json({"user": user});
+		})
+	})
+	
+	router.get('/rides', passport.authenticate('jwt', { session: false }), function(req, res) {
+		User.findOne({email: req.user.email}, function(err, user) {
+			if(err) throw err;
+			Ride.find({_id: user.pastRides}, function(err, rides) {
+				res.json({"rides": rides});
+			})
 		})
 	})
 
+	router.get('/favoriteBikes', passport.authenticate('jwt', { session: false }), function(req, res) {
+		User.findOne({email: req.user.email}, function(err, user) {
+			if(err) throw err;
+			Bike.find({number: user.favoriteBikes}, function(err, bikes) {
+				res.json({"bikes": bikes});
+			})
+		})
+	})
 	return router;
 }
